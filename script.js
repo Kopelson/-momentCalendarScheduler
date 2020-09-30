5d47cefed194f373d49be1489cfb3bf3ee297d4c
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-09-30 12:14:58 -0700

    created the createTimeBlocks function to create new time blocks for every hour in the day.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//JQuery will run this fucntionc on window load
+//JQuery will run this function on window load
 $(document).ready(function() {
     getCurrentDay();
     createTimeBlocks();
@@ -10,16 +10,38 @@ function getCurrentDay() {
     let currentDay = moment().format("dddd, MMMM Do");
     $('#currentDay').text(currentDay);
 }
-
+//creates new time blocks for the day scheduler 
 function createTimeBlocks() {
     let container = $(".container");
     let startOfDay = moment().startOf('day');
     let myTime = startOfDay.add(9, 'h').format("h:mma");
     for(let i = 0; i < 9; i++){
+        
+        //Create new variables for each time block
         let row = $("<div>");
-        row.attr("class", "row");
+        let hourSpan = $("<span>");
+        let textSpan = $("<span>");
+        let textArea = $("<textarea>");
+        let button = $("<button>");
+        let icon = $("<i>");
+        //set id attributes
         row.attr("id", i);
-        row.text(myTime);
+        //set class attributes
+        row.attr("class", "row time-block");
+        hourSpan.attr("class", "hour col-2");
+        button.attr("class", "saveBtn col-2");
+        icon.attr("class", "far fa-save ");
+        textSpan.attr("class", "col-8")
+        textArea.attr("class", "col-12")
+        //add texts to element
+        hourSpan.text(myTime);
+        //append each element in order of layout
+        button.append(icon);
+        textSpan.append(textArea);
+        row.append(hourSpan);
+        row.append(textSpan);
+        row.append(button);
+        //change time block by 1 hour
         startOfDay = startOfDay.add(1, 'h');
         myTime = startOfDay.format("h:mma");
         container.append(row);
