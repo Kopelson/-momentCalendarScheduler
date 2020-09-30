8480a791b853901ddfd66b8bd7958b13c8c3bb5d
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-09-30 15:27:06 -0700

    added a function that checks current time and updates time blocks classes to represent past, present, and future.

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 $(document).ready(function() {
     getCurrentDay();
     createTimeBlocks();
+    //change the style of each time block according to what time it is
+    updateTimeBlocks();
+    //fill in each time block with local storage data
+
+    //save each hour time block data to local storage
+    
 
 })
 
@@ -16,7 +22,6 @@ function createTimeBlocks() {
     let startOfDay = moment().startOf('day');
     let myTime = startOfDay.add(9, 'h').format("h:mma");
     for(let i = 0; i < 9; i++){
-        
         //Create new variables for each time block
         let row = $("<div>");
         let hourSpan = $("<span>");
@@ -25,14 +30,14 @@ function createTimeBlocks() {
         let button = $("<button>");
         let icon = $("<i>");
         //set id attributes
-        row.attr("id", i);
+        row.attr("value", i);
         //set class attributes
         row.attr("class", "row time-block");
         hourSpan.attr("class", "hour col-2");
         button.attr("class", "saveBtn col-2");
-        icon.attr("class", "far fa-save ");
-        textSpan.attr("class", "col-8")
-        textArea.attr("class", "col-12")
+        icon.attr("class", "far fa-save");
+        textSpan.attr("class", "textSpan col-8")
+        textArea.attr("class", "col-12 h-100")
         //add texts to element
         hourSpan.text(myTime);
         //append each element in order of layout
@@ -47,6 +52,40 @@ function createTimeBlocks() {
         container.append(row);
     };
 };
+//This function updates the time-blocks "textSpan" class to change colors based on time - past, present, future.
+function updateTimeBlocks(){
+    //this assigns hourBlock all elements with the class "textSpan"
+    let hourBlock = $('.textSpan');
+    //this assigns two variables a moment of time start of day
+    let startOfDay = moment().startOf('day');
+    let startOfDay2 = moment().startOf('day');
+    //this adds 8 hours to the first moment of time
+    let time = startOfDay.add(8, 'h');
+    //this adds 9 hours to the first moment of time
+    let time2 = startOfDay2.add(9, "h");
+    //this jquery each block will check the current time and compare it to the time that is associated with each time block, then assign a new class to the current time block textSpan either past, present, or future.
+    hourBlock.each(function(){
+        //this assigns adds a hour to the current time of the variable, the second time variable is to calculate the is between method that moment.js provides.
+        time = startOfDay.add(1,"h");
+        time2 = startOfDay2.add(1,"h");
+        //this variable stores a boolean value that checks if the time block is before the current time of day
+        let checkTime = moment(time).isBefore();
+        //this variable stores a boolean value that checks if the time block is between the hour time blocks
+        let currentTime = moment().isBetween(time, time2); 
+        //this if/else statements adds the logic is the time block in the past, present, or future.
+        if(checkTime && !currentTime){
+            //jquery addClass adds a new class "past" to the textSpan
+            $(this).addClass("past");
+        } else if (currentTime) {
+            //jquery addClass adds a new class "present" to the textSpan
+            $(this).addClass("present");
+        } 
+        else {
+            //jquery addClass adds a new class "future" to the textSpan
+            $(this).addClass("future");
+        }
+       
+    })
 
-    
+};    
 
